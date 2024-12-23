4cd7027a287fb5e9b5612c19665e5cd93a6ab950
Matheus-art66 <matheusmen58@gmail.com>
2024-12-23 15:43:17 -0300

    desafio

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("Ola mundo");
+const {input} = require('@inquirer/prompts')
+
+const start = async () => {
+    let nome = await input({ message: "Digite o nome do Herói: " });
+    let xp = await input({ message: "Digite o xp que possui: " });
+  
+    xp = parseInt(xp, 10);
+  
+    let nivel = '';
+    if (xp < 1000) {
+      nivel = "Ferro";
+    } else if (xp >= 1001 && xp <= 2000) {
+      nivel = "Bronze";
+    } else if (xp >= 2001 && xp <= 5000) {
+      nivel = "Prata";
+    } else if (xp >= 5001 && xp <= 7000) {
+      nivel = "Ouro";
+    } else if (xp >= 7001 && xp <= 8000) {
+      nivel = "Platina";
+    } else if (xp >= 8001 && xp <= 9000) {
+      nivel = "Ascendente";
+    } else if (xp >= 9001 && xp <= 10000) {
+      nivel = "Imortal";
+    } else {
+      nivel = "Radiante";
+    }
+  
+    console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
+  };
+  start()
